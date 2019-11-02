edfd1f5c07f86bf3e5eed41444d61464cd657930
killedmyself <markmisra@gmail.com>
2019-11-03 01:41:41 +0200

    Add map to tie up function calls

--- First/main.js
+++ First/main.js
@@ -1,60 +1,47 @@
 Submit_Button = document.getElementById("Submit");
 
 function kelvinToFC(Kelvin) {
-	var Fahrenheit = Kelvin * (9/5) - 459.67;
-	var Celsius = Kelvin - 273.15;
-	var result = [{'F':Fahrenheit.toPrecision(5)}, {'C': Celsius.toPrecision(5)}];
+	let Fahrenheit = Kelvin * (9/5) - 459.67;
+	let Celsius = Kelvin - 273.15;
+	let result = [{'F':Fahrenheit.toPrecision(5)}, {'C': Celsius.toPrecision(5)}];
 
-	var json = JSON.stringify(result);
-	
-	alert(json);
-
+	return result;
 }
 
 function fahrenheitToKC(Fahrenheit) {
-	var Kelvin = (Fahrenheit - 32) * (5/9) + 273.15;
-	var Celsius = (Fahrenheit - 32) * (5/9);
-	var result = [{'K':Kelvin.toPrecision(5)}, {'C': Celsius.toPrecision(5)}];
+	let Kelvin = (Fahrenheit - 32) * (5/9) + 273.15;
+	let Celsius = (Fahrenheit - 32) * (5/9);
+	let result = [{'K':Kelvin.toPrecision(5)}, {'C': Celsius.toPrecision(5)}];
 
-	var json = JSON.stringify(result);
+	return result
+}
+
+function celsiusToKF(Celsius) {
+	let Kelvin = Celsius + 273.15	;
+	let Fahrenheit = Celsius * 9/5 + 32;
+	let result = [{'K':Kelvin.toPrecision(5)}, {'F': Fahrenheit.toPrecision(5)}];
+
+	return result
+}
+
+const MAP_CONVERTOR = { 
+						'K': kelvinToFC , 
+						'F': fahrenheitToKC ,
+						'C': celsiusToKF	
+				  	  }
+
+function main() {
+	let Val = document.getElementById("Input").value;
+	Val.trim();
+
+	let digit = parseFloat(Val.slice(0, -1));
+	const symbol = (Val[Val.length - 1]).toUpperCase();
+
+
+	const result = MAP_CONVERTOR[symbol](digit) || "Invalid argument";
+	const json = JSON.stringify(result);
 	
 	alert(json);
 }
 
-function celsiusToKF(Celsius) {
-	var Kelvin = Celsius + 273.15	;
-	var Fahrenheit = Celsius * 9/5 + 32;
-	var result = [{'K':Kelvin.toPrecision(5)}, {'F': Fahrenheit.toPrecision(5)}];
-
-	var obj = JSON.stringify(result);
-	alert(obj);
-}
-
-Convertor = function() {
-	var Val = document.getElementById("Input").value;
-	Val.trim();
-
-	var digit = parseFloat(Val.slice(0, -1));
-	const symbol = (Val[Val.length - 1]).toUpperCase();
-
-	switch (symbol) {
-		case 'K':
-			kelvinToFC(digit);
-			break;
-		
-		case 'F':
-			fahrenheitToKC(digit);
-			break;
-		
-		case 'C':
-			celsiusToKF(digit);
-			break;
-		
-		default:
-			alert("Invalid argument");
-			break;
-	}
-			
-}
-
-Submit_Button.addEventListener("click",Convertor)
+Submit_Button.addEventListener("click", main)
