c4dbd37c680b8603af3c6fa43ef51614e43add8b
killedmyself <markmisra@gmail.com>
2019-11-04 14:16:15 +0200

    Add comments

--- Third/main.js
+++ Third/main.js
@@ -19,16 +19,17 @@ function main() {
 	
 	setUp();
 
-	if (first_player_probability < 0 || first_player_probability > 0.3) {
-		alert("Invalid argument for player 1!");
+	if (first_player_probability < 0.1 || first_player_probability > 0.3) {
+		alert("Invalid argument for player 1! Use values from range (0.1 - 0.3)");
 		return;
 	}
 
-	if (second_player_probability < 0 || second_player_probability > 0.3) {
-		alert("Invalid argument for player 2!");
+	if (second_player_probability < 0.1 || second_player_probability > 0.3) {
+		alert("Invalid argument for player 2! Use values from range (0.1 - 0.3)");
 		return;
 	}
 
+	// Get estimation of probability per step. 
 	const playersEstimationList = [
 									estimateProbabilityPerStep(first_player_probability, distance),
 									estimateProbabilityPerStep(second_player_probability, distance)
@@ -36,6 +37,7 @@ function main() {
 
 	const playerNumber = parseInt(prompt("Pick players number (1-2): "));
 
+	// if we pick player #1 then we compare chances-per-step with player#2 chances and when we get better chance we shoot
 	if (playerNumber == 1) {
 		for (var i = 0; i <= distance - 1; i++) {
             if ((playersEstimationList[0] * i + first_player_probability) > (1 - playersEstimationList[1] * (i + 1) - second_player_probability)) {
@@ -44,10 +46,12 @@ function main() {
             }
         }
 	} else if (playerNumber == 2) {
+		// Check if it is profitable to shoot from the first step 
 		if (playersEstimationList[1] > 1 - playersEstimationList[0] - first_player_probability) {
                 distance--;
                 alert("The optimal shot range is: " + distance);
         } else {
+        	// the same as we did for player one but one step less
         	for (var i = 0; i <= distance - 2; i++) {
                 if ((playersEstimationList[0] * i) + first_player_probability > (1 - playersEstimationList[1] * (i + 2) - second_player_probability)) {
                     alert("The optimal shot range is: " + i);
